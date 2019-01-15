3cc42dff4ceb41c1d0e75ac5a806b8701be0144f
Alek Snyder <asnyder0816@gmail.com>
2019-01-14 21:25:53 -0500

    List stations from line input instead of manual entry

--- src/command/list/index.js
+++ src/command/list/index.js
@@ -1,15 +1,6 @@
 import axios from 'axios';
-import { bold } from '../../utils/format';
-import { basicTable } from '../../utils/table';
-
-const vAlignCenter = columns =>
-  columns.map(column => {
-    if (typeof column === 'string') {
-      return { content: column, vAlign: 'center', hAlign: 'center' };
-    }
-
-    return { ...column, vAlign: 'center' };
-  });
+import inquirer from 'inquirer';
+import cli from '../../command';
 
 const stationsUrl =
   'https://api.wmata.com/Rail.svc/json/jStations?contentType=json&api_key=f640f6ee5156453b864f6582f585dd73';
@@ -43,64 +34,38 @@ const transformLine = line => {
 };
 
 export default function(name) {
-  getStations(stationsUrl).then(data => {
-    const abbrLine = transformLine(name);
-    const heading = name.toUpperCase();
+  getStations(stationsUrl)
+    .then(data => {
+      const abbrLine = transformLine(name);
+      const stationList = [];
 
-    const lineColor = data.filter(
-      color =>
-        color.LineCode4 === `${abbrLine}` ||
-        color.LineCode3 === `${abbrLine}` ||
-        color.LineCode2 === `${abbrLine}` ||
-        color.LineCode1 === `${abbrLine}`
-    );
+      const lineColor = data.filter(
+        color =>
+          color.LineCode4 === `${abbrLine}` ||
+          color.LineCode3 === `${abbrLine}` ||
+          color.LineCode2 === `${abbrLine}` ||
+          color.LineCode1 === `${abbrLine}`
+      );
 
-    const stationsTable = basicTable();
+      Object.keys(lineColor).forEach((key, index) => {
+        stationList[index] = lineColor[key].Name;
+      });
 
-    stationsTable.push([
-      { colSpan: 36, content: `${heading} Line`, hAlign: 'center' },
-    ]);
-    stationsTable.push(
-      vAlignCenter([
-        {
-          colSpan: 28,
-          content: bold('Name'),
-          hAlign: 'center',
-        },
-        {
-          colSpan: 8,
-          content: bold('Lines'),
-          hAlign: 'center',
-        },
-      ])
-    );
-
-    Object.keys(lineColor).forEach(key => {
-      const stationName = lineColor[key].Name;
-      let lines = [];
-
-      lines[0] = lineColor[key].LineCode1;
-      lines[1] = lineColor[key].LineCode2;
-      lines[2] = lineColor[key].LineCode3;
-      lines[3] = lineColor[key].LineCode4;
-
-      lines = lines.filter(line => line !== null).join();
-
-      stationsTable.push(
-        vAlignCenter([
+      return inquirer
+        .prompt([
           {
-            colSpan: 28,
-            content: `${stationName}`,
-          },
-          {
-            colSpan: 8,
-            content: `${lines}`,
-            hAlign: 'center',
+            type: 'list',
+            name: 'stations',
+            message: 'Select a station:',
+            choices: stationList,
+            default: [],
           },
         ])
-      );
+        .then(answer => {
+          cli.station(answer.stations);
+        });
+    })
+    .catch(error => {
+      console.log(error);
     });
-
-    console.log(stationsTable.toString());
-  });
 }
