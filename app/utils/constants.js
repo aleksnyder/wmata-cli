d04176f0aa93ec7c1cfaf0ed1f51bbc4e31e53dc
Alek Snyder <asnyder0816@gmail.com>
2023-11-14 13:00:26 -0500

    docs: clarify stance on keeping api key in vcs
    Refs: #21

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,7 +1,6 @@
 // Yes, adding the API key to version control is frowned
 // upon; but having a key for anyone who uses this package
-// without creating one for themselves is worthwhile since
-// I'm willing to let other use the same key.
+// without creating one for themselves is worthwhile.
 export const apiKey = 'f640f6ee5156453b864f6582f585dd73';
 
 export default { apiKey };
